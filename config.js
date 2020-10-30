d287a8c682e288b366506836e1ff827a3480fcbd
Anusha Nandam <anusha.nandam1@gmail.com>
2020-10-29 21:37:02 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// Our own Twitter App keys here
+module.exports = {
+  consumer_key:         'XAfUBCve1vNMmK0QjRp2vvir9',
+  consumer_secret:      'KmsCJU9XWODbPdw6tbRjCBjrvzEx8SzUdyocrfd3obd4HxRwhd',
+  access_token:         '1317203914509283328-EWXMuImGw5IRBwXJB1hnWAIhzjYszY',
+  access_token_secret:  'tH2F1mUoc06S5cOGrvY8Yq6z1difb9EjdhIfhf9Fi7Oj4'
+}
